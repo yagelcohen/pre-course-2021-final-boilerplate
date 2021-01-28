12ca7f062f9aaef377ed5b30285e6b09f7da0330
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-01-28 12:52:32 +0200

    add a counter tasks

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-//query elements and create some...
+//query elements and create some and Defining values..
 const addButton = document.querySelector('button');
 const ViewSection =  document.querySelector('section');
 const input = document.querySelector('input');
 const list = document.querySelector('ul');
 const listItem = document.createElement('li')
+let countTasks = 0;
+
 
 //function that add or delete the task and the significance of the task
-function whichTaskToAddOrDelete(textInput){
+function whichTaskToAddOrDeleteAndCountTask(){
     let significance = document.getElementById("priority-selector").value;
     let insideTheInput =significance + ' ' + input.value;
-    input.value=' ';
+    input.value='';
     const listItem=document.createElement('li')
     const buttonDelete = document.createElement('button');
     const span=document.createElement('span')
@@ -17,16 +19,21 @@ function whichTaskToAddOrDelete(textInput){
     listItem.append(span); listItem.append(buttonDelete);
     buttonDelete.innerText=' done! :)';
     list.append(listItem);
+    countTasks++;
+    document.getElementById('counter').innerHTML = countTasks;
     buttonDelete.onclick = function(e){
        list.removeChild(listItem)
+    countTasks--;
+    document.getElementById('counter').innerHTML = countTasks;
     }
     input.focus();
 }
+
 //to order the button that when we click on him do this function.
-addButton.addEventListener('click', whichTaskToAddOrDelete);
+addButton.addEventListener('click', whichTaskToAddOrDeleteAndCountTask);
 
 //to order the input that when we keypress on the enter key do this function.
 input.addEventListener('keypress',  (e) => {
     if (e.key === 'Enter')
-    {whichTaskToAddOrDelete()}
+    {whichTaskToAddOrDeleteAndCountTask()}
 });
