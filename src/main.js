3b497f90e2fad42895d7d4581d1ecbf9a169497f
yagelcohen <77248177+yagelcohen@users.noreply.github.com>
2021-02-01 19:24:46 +0200

    creat a updatebutton

--- src/main.js
+++ src/main.js
@@ -18,7 +18,9 @@ const ViewSection =  document.querySelector('section');
 const input = document.querySelector('input');
 const list = document.querySelector('ul');
 let countTasks = 0;
+let significance =document.getElementById("priority-selector").value;
 countData = 0;//-the key of the data
+lengthKeys = 0;
 keys = Object.keys(localStorage);//-a array of the keys of the data
 
 
@@ -26,6 +28,10 @@ keys = Object.keys(localStorage);//-a array of the keys of the data
   const listItem = document.createElement("li");
   const buttonDelete = document.createElement('button');
   buttonDelete.classList.add("buttonDelete");
+  buttonDelete.innerHTML = '<button class = "fa fa-trash fa-2x"></button>';
+  const buttonUpdate = document.createElement('button');
+  buttonUpdate.classList.add('button');
+  buttonUpdate.classList.add('buttonUpdate');
   const todoContainer = document.createElement('div');
   todoContainer.classList.add("todo-container");
   const todoPriority = document.createElement('div');
@@ -46,41 +52,64 @@ keys = Object.keys(localStorage);//-a array of the keys of the data
   input.value='';
   listItem.append(todoContainer);
   listItem.append(" " + " ")
+  buttonUpdate.innerText = "Update";
   listItem.append(buttonDelete);
-listItem.setAttribute("id",id);
+  listItem.append(buttonUpdate);
+  listItem.setAttribute('id',id);
   list.append(listItem);
-  buttonDelete.innerText='x';
   countTasks++;
   document.getElementById('counter').innerHTML = countTasks;
   buttonDelete.onclick = function(e){
-localStorage.removeItem(keys[listItem.getAttribute('id')]);
-      list.removeChild(listItem);
-countTasks--;
-document.getElementById('counter').innerHTML = countTasks;
+  localStorage.removeItem(listItem.getAttribute('id'));
+  list.removeChild(listItem);
+  countTasks--;
+  document.getElementById('counter').innerHTML = countTasks;
   }
+  buttonUpdate.onclick = function(e){
+    if (input.value !== '' && Number(significance)){
+    countTasks--;
+    list.removeChild(listItem);
+    let significance =document.getElementById("priority-selector").value;
+    whichTaskToAddOrDeleteAndCountTask(significance,new Date().toLocaleString(),input.value,listItem.getAttribute('id'))
+    }
+    else{
+    alert("Make sure you insert data");
+    }
+    }
+  input.value='';
   input.focus();
  }
- if (localStorage.length !== 0) {  
-  for(let i = 0 ; i < localStorage.length;i++){ 
+
+ if (localStorage.length !== 0) {
+  lengthKeys = keys.length;
+  minkeys = keys.sort();
+  for(let i = 0 ; i < localStorage.length;i++){
   Data = localStorage.key(i);
   Data = JSON.parse(localStorage.getItem(Data));
-  whichTaskToAddOrDeleteAndCountTask(Data.significance,Data.data,Data.Text,countTasks);
+  whichTaskToAddOrDeleteAndCountTask(Data.significance,Data.data,Data.Text,minkeys[i]);
   }
   countData = bigInArray(keys);
   }
   //to order the button that when we click on him do this function.
   //function that add the information from the user to localStorage and to the TaskManger
   if(document.getElementById("priority-selector").value !== null){
-  document.getElementById("add-button").onclick = function() {
-  let significance =document.getElementById("priority-selector").value;
-  var obj =  JSON.stringify({ significance: significance, data: new Date().toLocaleString(), Text: input.value });
-  localStorage.setItem(countData, obj);
-  whichTaskToAddOrDeleteAndCountTask(significance,new Date().toLocaleString(),input.value,countData);
-  countData++;
-  };
-  }
+    document.getElementById("add-button").onclick = function() {
+    let significance =document.getElementById("priority-selector").value;
+    if( Number(significance) && input.value !== ''){
+    let obj =  JSON.stringify({ significance: significance, data: new Date().toLocaleString(), Text: input.value });
+    console.log('the keys = ' , keys, 'the key(countData)=',countData);
+    localStorage.setItem(countData, obj);
+    whichTaskToAddOrDeleteAndCountTask(significance,new Date().toLocaleString(),input.value,countData);
+    countData++;
+    keys = Object.keys(localStorage);
+    lengthKeys = keys.length;
+    }
+    else{
+    alert("Make sure you not missing data");
+    }
+    };
  //to order the input that when we keypress on the enter key do this function.
- if(input){
+ if(significance !== "--Select the level of significance of your task--" && input.value!== ""){
   input.addEventListener('keypress',  (e) => {
   if (e.key === 'Enter')
   {whichTaskToAddOrDeleteAndCountTask()}
@@ -107,9 +136,5 @@ function sortList (){
       switching = true;
     }
   }
+} 
 }
-
-
-//to order the button that when we click on him do this function.
-// addButton.addEventListener('click', whichTaskToAddOrDeleteAndCountTask);
-
